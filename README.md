781ee06793f77295e88a4fda941f5ec8b0576263
zrshishir <zrshishir@gmail.com>
2023-05-28 20:28:35 +0600

    readme file updated

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-Docker for WP multisite
+# wordpress-phpmyadmin-mariadb on macbook m1
+
+- clone this git repo `git clone git@github.com:emranio/docker-wordpress-multisite.git`. Run the following command to run the wordpress environment on docker
+
+    'docker compose up -d'
+
+- to run all containers of the `docker-compose.yml` file perfectly,  make available your computer `80` ports.
+- If you want to change the password of your database change it on environment section of `wp` services of `docker-compose-yml` file
+
+    ```
+    environment:
+      - WORDPRESS_DB_HOST=db
+      - WORDPRESS_DB_NAME=test
+      - WORDPRESS_DB_USER=root
+      - WORDPRESS_DB_PASSWORD=test
+      - WORDPRESS_LOCAL_DEV=true
+      - WORDPRESS_DEBUG=true
+    ```
+
+
+- also on db environment section
+
+    ```
+    environment:
+      - MYSQL_DATABASE=test
+      - MYSQL_ROOT_PASSWORD=test
+      - MYSQL_USER=root
+    ```
+
+- If you face `Error establishing a database connection`, delete your `wp-config.php` file and reload.
+
+### Multi site configuration
+
+- Go to `setting->permalinks` and set the permalink structure as `Post name` and save it.
+- Open `wp-config.php` file and paste the constants `define( 'WP_ALLOW_MULTISITE', true );` after the `WP-DEBUG` section and save it.
+- Now reload your wp-admin.
+- Go to `Tools->Network Setup` choose sub directory options and fill the fields and click on `install` button.
+- Copy the following code and paste it on `wp-config.php` file after `WP_ALLOW_MULTISITE` constant.
+
+  ```
+  define( 'MULTISITE', true );
+  define( 'SUBDOMAIN_INSTALL', false );
+  define( 'DOMAIN_CURRENT_SITE', 'localhost' );
+  define( 'PATH_CURRENT_SITE', '/' );
+  define( 'SITE_ID_CURRENT_SITE', 1 );
+  define( 'BLOG_ID_CURRENT_SITE', 1 );
+  ```
+
+- Copy the following code and select all and paste it on `.htaccess` file which is on your wordpress root directory.
+
+ ```
+  RewriteEngine On
+  RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
+  RewriteBase /
+  RewriteRule ^index\.php$ - [L]
+
+  # add a trailing slash to /wp-admin
+  RewriteRule ^([_0-9a-zA-Z-]+/)?wp-admin$ $1wp-admin/ [R=301,L]
+
+  RewriteCond %{REQUEST_FILENAME} -f [OR]
+  RewriteCond %{REQUEST_FILENAME} -d
+  RewriteRule ^ - [L]
+  RewriteRule ^([_0-9a-zA-Z-]+/)?(wp-(content|admin|includes).*) $2 [L]
+  RewriteRule ^([_0-9a-zA-Z-]+/)?(.*\.php)$ $2 [L]
+  RewriteRule . index.php [L]
+  ```
+- Now reload your admin panel you will see multi site menu on your left top bar beside the wordpress home menu button. 
+- to development purpose keep all your plugins within the `wp-content/plugins` directory or wordpress plugin directory.
+- Now reload and make your site as much as you wish.
+
+##                        Happy Coding.......
+
--- data/wp-config.php
+++ data/wp-config.php
@@ -20,22 +20,22 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'test');
+define('DB_NAME', 'test');
 
 /** Database username */
-define( 'DB_USER', 'root');
+define('DB_USER', 'root');
 
 /** Database password */
-define( 'DB_PASSWORD', 'test');
+define('DB_PASSWORD', 'test');
 
 /** Database hostname */
-define( 'DB_HOST', 'db');
+define('DB_HOST', 'db');
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define('DB_CHARSET', 'utf8');
 
 /** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+define('DB_COLLATE', '');
 
 /**#@+
  * Authentication unique keys and salts.
@@ -48,14 +48,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define('AUTH_KEY', 'put your unique phrase here');
+define('SECURE_AUTH_KEY', 'put your unique phrase here');
+define('LOGGED_IN_KEY', 'put your unique phrase here');
+define('NONCE_KEY', 'put your unique phrase here');
+define('AUTH_SALT', 'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT', 'put your unique phrase here');
+define('NONCE_SALT', 'put your unique phrase here');
 
 /**#@-*/
 
@@ -79,74 +79,67 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define('WP_DEBUG', true);
 
 // If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
 // see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-	$_SERVER['HTTPS'] = 'on';
+    $_SERVER['HTTPS'] = 'on';
 }
 
 @ini_set('session.cookie_httponly', true);
- @ini_set('session.cookie_secure', true);
- @ini_set('session.use_only_cookies', true);
- define('WP_MEMORY_LIMIT', '256M');
- define( 'WP_MAX_MEMORY_LIMIT', '256M' );
+@ini_set('session.cookie_secure', true);
+@ini_set('session.use_only_cookies', true);
+define('WP_MEMORY_LIMIT', '256M');
+define('WP_MAX_MEMORY_LIMIT', '256M');
 @ini_set('session.cookie_httponly', true);
- @ini_set('session.cookie_secure', true);
- @ini_set('session.use_only_cookies', true);
- define('WP_MEMORY_LIMIT', '256M');
- define( 'WP_MAX_MEMORY_LIMIT', '256M' );
+@ini_set('session.cookie_secure', true);
+@ini_set('session.use_only_cookies', true);
+define('WP_MEMORY_LIMIT', '256M');
+define('WP_MAX_MEMORY_LIMIT', '256M');
 @ini_set('session.cookie_httponly', true);
- @ini_set('session.cookie_secure', true);
- @ini_set('session.use_only_cookies', true);
- define('WP_MEMORY_LIMIT', '256M');
- define( 'WP_MAX_MEMORY_LIMIT', '256M' );
+@ini_set('session.cookie_secure', true);
+@ini_set('session.use_only_cookies', true);
+define('WP_MEMORY_LIMIT', '256M');
+define('WP_MAX_MEMORY_LIMIT', '256M');
 @ini_set('session.cookie_httponly', true);
- @ini_set('session.cookie_secure', true);
- @ini_set('session.use_only_cookies', true);
- define('WP_MEMORY_LIMIT', '256M');
- define( 'WP_MAX_MEMORY_LIMIT', '256M' );
+@ini_set('session.cookie_secure', true);
+@ini_set('session.use_only_cookies', true);
+define('WP_MEMORY_LIMIT', '256M');
+define('WP_MAX_MEMORY_LIMIT', '256M');
 /* Add any custom values between this line and the "stop editing" line. */
 
-
-
 @ini_set('session.cookie_httponly', true);
- @ini_set('session.cookie_secure', true);
- @ini_set('session.use_only_cookies', true);
- define('WP_MEMORY_LIMIT', '256M');
- define( 'WP_MAX_MEMORY_LIMIT', '256M' );
+@ini_set('session.cookie_secure', true);
+@ini_set('session.use_only_cookies', true);
+define('WP_MEMORY_LIMIT', '256M');
+define('WP_MAX_MEMORY_LIMIT', '256M');
 @ini_set('session.cookie_httponly', true);
- @ini_set('session.cookie_secure', true);
- @ini_set('session.use_only_cookies', true);
- define('WP_MEMORY_LIMIT', '256M');
- define( 'WP_MAX_MEMORY_LIMIT', '256M' );
+@ini_set('session.cookie_secure', true);
+@ini_set('session.use_only_cookies', true);
+define('WP_MEMORY_LIMIT', '256M');
+define('WP_MAX_MEMORY_LIMIT', '256M');
 @ini_set('session.cookie_httponly', true);
- @ini_set('session.cookie_secure', true);
- @ini_set('session.use_only_cookies', true);
- define('WP_MEMORY_LIMIT', '256M');
- define( 'WP_MAX_MEMORY_LIMIT', '256M' );
+@ini_set('session.cookie_secure', true);
+@ini_set('session.use_only_cookies', true);
+define('WP_MEMORY_LIMIT', '256M');
+define('WP_MAX_MEMORY_LIMIT', '256M');
 @ini_set('session.cookie_httponly', true);
- @ini_set('session.cookie_secure', true);
- @ini_set('session.use_only_cookies', true);
- define('WP_MEMORY_LIMIT', '256M');
- define( 'WP_MAX_MEMORY_LIMIT', '256M' );
+@ini_set('session.cookie_secure', true);
+@ini_set('session.use_only_cookies', true);
+define('WP_MEMORY_LIMIT', '256M');
+define('WP_MAX_MEMORY_LIMIT', '256M');
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+if (!defined('ABSPATH')) {
+    define('ABSPATH', __DIR__ . '/');
 }
 
-
-define( 'MULTISITE', true );
-define( 'SUBDOMAIN_INSTALL', false );
-define( 'DOMAIN_CURRENT_SITE', 'localhost' );
-define( 'PATH_CURRENT_SITE', '/' );
-define( 'SITE_ID_CURRENT_SITE', 1 );
-define( 'BLOG_ID_CURRENT_SITE', 1 );
-
-
+define('ADMIN_COOKIE_PATH', '/');
+define('COOKIEPATH', '');
+define('SITECOOKIEPATH', '');
+define('COOKIE_DOMAIN', false);
 
 
 /** Sets up WordPress vars and included files. */
